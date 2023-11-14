93f6d92603c7283baae2049f4e666e48fbdf9eb6
amitkumardianapps <140390725+amitkumardianapps@users.noreply.github.com>
2023-11-14 18:40:48 +0530

    Updated use of pojo for the dataprovider

--- src/test/java/dataprovider/CartData.java
+++ src/test/java/dataprovider/CartData.java
@@ -5,9 +5,9 @@ import org.testng.annotations.DataProvider;
 public class CartData {
 
   @DataProvider(name = "CartData")
-  public Object[][] cartData() {
-    return new Object[][] {
-      {"standard_user", "secret_sauce","Sauce Labs Backpack","Amit", "Kumar", "10001"},
+  public static Object[][] cartData() {
+    return new Object[][]{
+            {new CartTestData("standard_user", "secret_sauce", "Sauce Labs Backpack", "Amit", "Kumar", "10001")},
     };
   }
 }
--- src/test/java/tests/EndToEndCheckoutTest.java
+++ src/test/java/tests/EndToEndCheckoutTest.java
@@ -2,6 +2,7 @@ package tests;
 
 import constants.SetupConstants;
 import dataprovider.CartData;
+import dataprovider.CartTestData;
 import helpers.BrowserSetup;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeClass;
@@ -22,21 +23,19 @@ public class EndToEndCheckoutTest {
 
   @Test(
           dataProvider = "CartData",
-      dataProviderClass = CartData.class,
-      groups = "regression")
-  public static void endToEndCheckout(String username, String password, String itemToAdd, String firstName, String lastName, String zipCode) {
-    CartOperations cartOperations=new CartOperations();
-    LoginOperations loginOperations=new LoginOperations();
-    LoginOperations.login(username, password);
-    ProductsOperations.addToCart(
-            new String[] {
-                    itemToAdd
-            });
+          dataProviderClass = CartData.class,
+          groups = "regression")
+  public static void endToEndCheckout(CartTestData testData) {
+    CartOperations cartOperations = new CartOperations();
+    LoginOperations loginOperations = new LoginOperations();
+    LoginOperations.login(testData.getUsername(), testData.getPassword());
+    ProductsOperations.addToCart(new String[]{testData.getItemToAdd()});
     CartOperations.clickCart();
     CartOperations.clickCheckout();
-    CartOperations.enterUserInformation(firstName, lastName, zipCode);
-    CartOperations.verifyAddedItems(new String[] {itemToAdd});
+    CartOperations.enterUserInformation(testData.getFirstName(), testData.getLastName(), testData.getZipCode());
+    CartOperations.verifyAddedItems(new String[]{testData.getItemToAdd()});
   }
+
 
   @AfterSuite
   public static void suiteCompletion() {
